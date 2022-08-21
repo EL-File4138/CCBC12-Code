2c7a7183275946932367ddb7900ba451ecc168de
EL_File4138 <elfile4138@outlook.com>
2022-08-21 10:51:16 +0800

    feat: add fonts.py,Multiple_Sudoku.cpp(Unfinished)

---
+++ Fonts/Fonts.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("words.png", 0)
+
+template = cv2.imread('word.png', 0)
+
+w, h = template.shape[::-1]
+res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+threshold = 0.885
+loc = np.where(res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), -1)
+cv2.imshow('res', res)
+cv2.imshow('res', img)
+cv2.waitKey()
+cv2.imwrite("a.png", img)
---
+++ Multiple_Sudoku.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+const int standard_list[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+using namespace std;
+
+int sudoku_org[9][9];
+
+int sudokuR[9][9];
+int sudokuG[9][9];
+int sudokuB[9][9];
+
+int Color_Seperation() {
+
+}
+
+bool Eligiblity_Check(int draft_sudoku[9][9]) {
+    int H, W, Block;
+    for (H = 0; H <= 9; H++) {
+        int checklist[9] = {};
+        for (W = 0; W <= 9; W++) {
+            checklist[W] = draft_sudoku[H][W];
+        }
+        sort(checklist, checklist + 9);
+        if (equal(checklist, checklist + 8, standard_list, standard_list + 8)) {
+            continue;
+        }
+        else {
+            return false;
+            break;
+        }
+    }
+    for (W = 0; W <= 9; W++) {
+        int checklist[9] = {};
+        for (H = 0; H <= 9; H++) {
+            checklist[H] = draft_sudoku[W][H];
+        }
+        sort(checklist, checklist + 9);
+        if (equal(checklist, checklist + 8, standard_list, standard_list + 8)) {
+            continue;
+        }
+        else {
+            return false;
+            break;
+        }
+    }
+    return true;
+}
